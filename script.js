91073c2b74a5c6c8382d7c0ac709325ae20e1329
lexx54 <chapelexx54@gmail.com>
2020-12-13 13:08:47 -0400

    disabled finished

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const d=document,
   $defaultF=d.querySelector(".default-f"),
   $defaultLF=d.querySelector(".label-f"),
   $errorF=d.querySelector(".error-f"),
-  $errorL=d.querySelectorAll(".label-err");
-
-  $errorL[0].style.color=`#d32f2f`;
-
+  $errorL=d.querySelectorAll(".label-err"),
+  $disabledI=d.querySelectorAll(".disabled-in");
+  
+  d.addEventListener("DOMContentLoaded",e=>{
+    $errorL[0].style.color=`#d32f2f`;
+    $disabledI.forEach(ele=>ele.disabled=true);
+  })
   d.addEventListener("focusin",e=>{
     if (e.target===$defaultF)$defaultLF.style.color="#2962ff";
     if (e.target===$errorF) $errorL[1].style.color="#d32f2f";
